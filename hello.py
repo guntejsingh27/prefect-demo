950380fb6c0ee649214742b21a4969fc5e7ed0d2
gunwt43 <guntej7@gmail.com>
2024-09-21 22:37:33 +1000

    first commit

---
+++ hello.py
@@ -0,0 +1,27 @@
+from prefect import flow, task
+from prefect.blocks.system import String
+
+string_block = String.load("redback-operations-trial")
+
+@task
+def create_message():
+    msg = string_block.value
+    return(msg)
+
+@flow
+def something_else():
+    result = 10
+    return(result)
+
+@flow
+def hello_world():
+    sub_flow_message = something_else()
+    task_message = create_message()
+    new_message = task_message + str(sub_flow_message)
+    print(new_message)
+
+if __name__ == "__main__":
+   hello_world.serve(name="my-first-deployment",
+                      tags=["onboarding"],
+                      parameters={"goodbye": True},
+                      interval=60)
